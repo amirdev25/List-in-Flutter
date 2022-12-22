86445aadcd7c9f1e233617ca3f808c810d42ffe3
Og'abek <amirdev25@gmail.com>
2022-12-22 12:30:47 +0500

    Date:22.12.2022

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "List Application",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+      body: BodyPage(),
+    );
+  }
+}
+
+class BodyPage extends StatefulWidget {
+  const BodyPage({Key? key}) : super(key: key);
+
+  @override
+  State<BodyPage> createState() => _BodyPageState();
+}
+
+class _BodyPageState extends State<BodyPage> {
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }
